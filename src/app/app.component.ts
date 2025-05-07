1245761825ff8044e711a57974b1f5ea06cf8d90
Damian Tapia <damian.tapia45@gmail.com>
2025-05-06 19:28:16 -0600

    initial commit with project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, Injector, ViewChild } from '@angular/core'
+
+import { createEditor } from './rete'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css', './common.css']
 })
-export class AppComponent {
-  title = 'rete-test';
+export class AppComponent implements AfterViewInit {
+  title = 'angular'
+  @ViewChild('rete') container!: ElementRef<HTMLElement>
+
+  constructor(private injector: Injector) { }
+
+  async ngAfterViewInit() {
+    await createEditor(this.container.nativeElement, this.injector)
+  }
 }
---
+++ src/app/customization/custom-node/custom-node.component.ts
@@ -0,0 +1,47 @@
+import {
+  Component,
+  Input,
+  HostBinding,
+  ChangeDetectorRef,
+  OnChanges,
+} from '@angular/core';
+import { ClassicPreset } from 'rete';
+import { CommonModule } from '@angular/common';
+import { RefDirective } from 'rete-angular-plugin/19';
+
+@Component({
+  imports: [CommonModule, RefDirective],
+  templateUrl: './custom-node.component.html',
+  styleUrls: ['./custom-node.component.sass'],
+  host: {
+    'data-testid': 'node',
+  },
+})
+export class CustomNodeComponent implements OnChanges {
+  @Input() data!: ClassicPreset.Node;
+  @Input() emit!: (data: any) => void;
+  @Input() rendered!: () => void;
+
+  seed = 0;
+
+  @HostBinding('class.selected') get selected() {
+    return this.data.selected;
+  }
+
+  constructor(private cdr: ChangeDetectorRef) {
+    this.cdr.detach();
+  }
+
+  ngOnChanges(): void {
+    this.cdr.detectChanges();
+    requestAnimationFrame(() => this.rendered());
+    this.seed++; // force render sockets
+  }
+
+  sortByIndex(a: any, b: any) {
+    const ai = a.value.index || 0;
+    const bi = b.value.index || 0;
+
+    return ai - bi;
+  }
+}
